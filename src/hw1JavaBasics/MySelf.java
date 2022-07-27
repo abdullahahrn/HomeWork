40ab63930c7374979692d40defe6092aae032617
ABDULLAHAHRN <abdullahahrn@gmail.com>
2022-07-26 22:20:27 -0400

    UPDATE

---
+++ src/hw1JavaBasics/MySelf.java
@@ -0,0 +1,15 @@
+package hw1JavaBasics;
+
+/*
+ * naming convention of Project
+ * package and class
+ */
+public class MySelf {
+//	i can write main method by a short cut using "Control+space"
+//  main method is the entry point of java..
+	public static void main(String[] args) {
+		System.out.println("Abdullah Al Harun");
+		System.out.println("1202 Leland Ave is My address");
+
+	}
+}
